50b9d445ecbee703c816a81399f0ff5b8e837d80
kai zhou <zhoukai@kaochong.com>
2017-02-10 18:31:02 +0800

    remove unused files

--- Example/Pods/Target Support Files/UICollectionViewFreezeLayout/UICollectionViewFreezeLayout-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double UICollectionViewFreezeLayoutVersionNumber;
-FOUNDATION_EXPORT const unsigned char UICollectionViewFreezeLayoutVersionString[];
-
